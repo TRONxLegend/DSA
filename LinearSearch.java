4bb01717e9e5e59bf2ea5b495da41015341e24c1
Ayush Singh <ayush.singh.csbs22@ggits.net>
2024-11-14 19:04:18 +0530

    first commit

---
+++ LinearSearch.java
@@ -0,0 +1,23 @@
+import java.util.*; 
+public class LinearSearch {
+    public static int linearsearch(int number[], int key){
+        for (int i = 0; i<number.length; i++){
+            if (number[i] == key){
+                return i;
+            }  
+        }
+        return -1;   
+    }
+    public static void main(String[] args){
+    int number[] ={1,3,4,6,7};
+    int key = 6;
+    int index = linearsearch(number, key);
+    if (index == -1){ 
+        System.out.println("Key Not Found");
+    }
+    else{
+        System.out.println("Key Found at index: " + index);
+    }
+}
+}
+
