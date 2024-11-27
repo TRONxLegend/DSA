1c83932e27ce221f18619ad2522dd8e321b584dd
Ayush Singh <130851856+TRONxLegend@users.noreply.github.com>
2024-11-27 21:21:24 +0530

    Update primenumber.java

--- primenumber.java
+++ primenumber.java
@@ -1,6 +1,14 @@
-public class primenumber {
-    public static void main(String[] args) {
-        
+    public static boolean isprimenumber{
+        int isprimenumber = true;
+        for(int i = 2; i<=n-1;i++){
+            if(n%1==o){
+                isprimenumber = false;
+            }
+        }
+        return false;
     }
-    
+    public static void main(String[] args) {
+        system.out.println(isprimenumber);
+    }
+   
 }
