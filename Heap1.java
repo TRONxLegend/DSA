c65ab52e3246447a84183670025569fda83af2e0
Ayush Singh <ayush.singh.csbs22@ggits.net>
2025-04-17 20:54:43 +0530

    PQ-For-Objects

--- Heap1.java
+++ Heap1.java
@@ -1,15 +1,52 @@
-import java.util.*;
-public class Heap1 {
-    public static void main(String args[]){
-        PriorityQueue<Integer> pg = new PriorityQueue<>();
-        pg.add(1);
-        pg.add(2);
-        pg.add(3);
-        pg.add(4);
-        pg.add(5);
-        pg.remove(3);
+// import java.util.*;
+// public class Heap1 {
+//     public static void main(String args[]){
+//         PriorityQueue<Integer> pg = new PriorityQueue<>();
+       
+//         pg.add(4);
+//         pg.add(5);
+//         pg.add(1);
+//         pg.add(2);
+//         pg.add(3);
 
-        System.out.println("Priority Queue: " + pg);
-        System.out.println("After removing 3: " + pg);
+//         // System.out.println("Priority Queue: " + pg);
+//         // System.out.println("After removing 3: " + pg);
+
+//         while(!pg.isEmpty()){
+//             System.out.println(pg.peek());
+//             pg.remove();
+//         }
+//     }
+// }
+
+
+             // PQ For Objects
+
+import java.util.*;
+public class Heap1{
+    static class Student implements Comparable<Student>{
+        String name;
+        int rank;
+        public Student(String name, int rank){
+            this.name = name;
+            this.rank = rank;
+        }
+     @Override
+        public int compareTo(Student s){
+            return this.rank - s.rank;
+        }
+    }
+    public static void main(String args[]){
+        PriorityQueue<Student> pq = new PriorityQueue<>();
+       pq.add(new Student("Ayush",34));
+       pq.add(new Student("Anukriti",23));
+         pq.add(new Student("Lakhan",12));
+         pq.add(new Student("Anu",45));
+
+
+        while(!pq.isEmpty()){
+            System.out.println(pq.peek().name + " " + pq.peek().rank);
+            pq.remove();
+        }
     }
 }
