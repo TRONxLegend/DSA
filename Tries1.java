d176be5206f17fd3c486b612b7d60e47bb126171
Ayush Singh <ayush.singh.csbs22@ggits.net>
2025-04-23 13:11:38 +0530

    Added:Tries1-File

---
+++ Tries1.java
@@ -0,0 +1,34 @@
+//  Insert in Tries;
+
+public class Tries1{
+    static class Node{
+        Node children[] = new Node[26];
+        boolean eow = false;
+   
+    Node(){
+        for(int i=0;i<26;i++){
+            children[i] = null;
+        }
+    }
+}
+public static Node root = new Node();
+public static void insert(String words){// TC -> O(L) where L is the length of the word:)
+    Node curr = root;
+    for(int level= 0; level<words.length(); level++){
+        int idx= words.charAt(level)-'a';
+        if(curr.children[idx]== null){
+            curr.children[idx] = new Node();
+        }
+        curr = curr.children[words.charAt(level)-'a'];
+    }
+    curr.eow = true;
+    }
+
+public static void main(String[] args){
+ String words[] = {"the", "a", "there", "answer", "any", "by", "bye", "their"};
+ for(int i=0;i<words.length;i++){
+    insert(words[i]);
+ }
+
+}
+}
