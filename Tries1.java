fa213265849bb9a69524ddb47a198e1f0361aec6
Ayush Singh <ayush.singh.csbs22@ggits.net>
2025-04-23 23:01:16 +0530

    Search-in-Tries

--- Tries1.java
+++ Tries1.java
@@ -1,34 +1,91 @@
-//  Insert in Tries;
+// //  Insert in Tries;
 
-public class Tries1{
-    static class Node{
-        Node children[] = new Node[26];
-        boolean eow = false;
+// public class Tries1{
+//     static class Node{
+//         Node children[] = new Node[26];
+//         boolean eow = false;
    
-    Node(){
-        for(int i=0;i<26;i++){
-            children[i] = null;
-        }
-    }
-}
-public static Node root = new Node();
-public static void insert(String words){// TC -> O(L) where L is the length of the word:)
-    Node curr = root;
-    for(int level= 0; level<words.length(); level++){
-        int idx= words.charAt(level)-'a';
-        if(curr.children[idx]== null){
-            curr.children[idx] = new Node();
-        }
-        curr = curr.children[words.charAt(level)-'a'];
-    }
-    curr.eow = true;
-    }
+//     Node(){
+//         for(int i=0;i<26;i++){
+//             children[i] = null;
+//         }
+//     }
+// }
+// public static Node root = new Node();
+// public static void insert(String words){// TC -> O(L) where L is the length of the word:)
+//     Node curr = root;
+//     for(int level= 0; level<words.length(); level++){
+//         int idx= words.charAt(level)-'a';
+//         if(curr.children[idx]== null){
+//             curr.children[idx] = new Node();
+//         }
+//         curr = curr.children[words.charAt(level)-'a'];
+//     }
+//     curr.eow = true;
+//     }
 
-public static void main(String[] args){
- String words[] = {"the", "a", "there", "answer", "any", "by", "bye", "their"};
- for(int i=0;i<words.length;i++){
-    insert(words[i]);
- }
+// public static void main(String[] args){
+//  String words[] = {"the", "a", "there", "answer", "any", "by", "bye", "their"};
+//  for(int i=0;i<words.length;i++){
+//     insert(words[i]);
+//  }
 
-}
-}
+// }
+// }
+
+
+
+
+
+// Search in Tries:)
+
+
+
+// public class Tries1{
+//     static class Node{
+//         Node children[] = new Node[26];
+//         boolean eow = false;
+//         Node(){
+//             for(int i =0; i<26;i++){
+//                 children[i] = null;
+
+//             }
+
+//         }
+//     }
+//     public static Node root = new Node();
+//     public static void insert(String words){// TC -> O(L) where L is the length of the word:)
+//         Node curr = root;
+//         for(int level=0; level<words.length();level++){
+//             int idx = words.charAt(level)-'a';
+//             if(curr.children[idx]==null){
+//                 curr.children[idx]=new Node();
+//             }
+//             curr = curr.children[idx];
+//         }
+//         curr.eow = true;
+//     }
+//     public static boolean search(String key){
+//         Node curr = root;
+//         for(int level=0;level<key.length();level++){
+//             int idx = key.charAt(level)-'a';
+//             if(curr.children[idx]==null){
+//                 return false;
+//             }
+//             curr = curr.children[idx];
+//         }
+//         return curr.eow == true;
+
+//         }
+    
+//     public static void main(String[] args){
+//         String words[] = {"the", "a", "there", "answer", "any", "by", "bye", "their"};
+       
+//         for(int i=0;i<words.length;i++){
+//             insert(words[i]);
+//         }
+        
+//         System.out.println(search("any"));
+//         System.out.println(search("hello"));
+//     }
+// }
