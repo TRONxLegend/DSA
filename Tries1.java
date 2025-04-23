9e108c001f19eefd37b5c08c05807efa76606c04
Ayush Singh <ayush.singh.csbs22@ggits.net>
2025-04-23 23:34:43 +0530

    Word-Break-Problem

--- Tries1.java
+++ Tries1.java
@@ -89,3 +89,74 @@
 //         System.out.println(search("hello"));
 //     }
 // }
+
+
+
+
+//    WORD BREApublic class Tries1{
+
+
+public class Tries1{
+    static class Node{
+        Node children[] = new Node[26];
+        boolean eow = false;
+       public Node(){
+            for(int i =0; i<26;i++){
+                children[i] = null;
+
+            }
+
+        }
+    }
+    public static Node root = new Node();
+    public static void insert(String words){// TC -> O(L) where L is the length of the word:)
+        Node curr = root;
+        for(int level=0; level<words.length();level++){
+            int idx = words.charAt(level)-'a';
+            if(curr.children[idx]==null){
+                curr.children[idx]=new Node();
+            }
+            curr = curr.children[idx];
+        }
+        curr.eow = true;
+    }
+    public static boolean search(String key){
+        Node curr = root;
+        for(int level=0;level<key.length();level++){
+            int idx = key.charAt(level)-'a';
+            if(curr.children[idx]==null){
+                return false;
+            }
+            curr = curr.children[idx];
+        }
+        return curr.eow == true;
+
+        }
+    
+        public static boolean wordBreak(String key){
+            if(key.length() == 0){
+                return true;
+            }
+         
+            for(int i=1;i<=key.length();i++){
+                /// substring(0,i) -> i is the length of the string
+               if(search( key.substring(0, i)) && wordBreak(key.substring(i))){
+                return true;
+               }
+                
+            }
+            return false;
+        }
+    public static void main(String[] args){
+        String words[] = {"i","like","sam","samsung","moblie","ice","cream"};
+       
+        for(int i=0;i<words.length;i++){
+            insert(words[i]);
+        }
+
+        String key = "ilikesamsung";
+        
+       System.out.println(wordBreak(key));
+        
+}
+}  
