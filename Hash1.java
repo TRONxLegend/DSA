882f1644773a7058b14bf09076ef40cb077eb0c8
Ayush Singh <ayush.singh.csbs22@ggits.net>
2025-05-01 00:21:53 +0530

    Iteration on Hash Map

--- Hash1.java
+++ Hash1.java
@@ -1,31 +1,53 @@
 // Operations of Hash Map:-  
 
-// 1. put() - To insert a key-value pair into the hash map.
+// import java.util.*;
+// public class Hash1 {
+//     public static void main(String[] args){
+//         HashMap<String, Integer > map = new HashMap<>();// Creation of HashMap
+//         // Insertion of key-value pairs :) put() method
+//         // T.C :) O(1) on average case and O(n) in worst case.
+//         map.put("Ayush",1);
+//         map.put("Anukriti",2);
+//         map.put("Lakhan",3);
+//         map.put("Anu",4);
+//         System.out.println(map);
+
+//         // get() - To retrieve the value associated with a specific key.
+//         // T.C :) (1);
+//         int data = map.get("Ayush");
+//         System.out.println(data);
+
+//         // containsKey() - To check if a specific key exists in the hash map.
+//         // T.C :) O(1);
+//         boolean check = map.containsKey("Anukriti");
+//         System.out.println(check);
+
+//         // remove() - To remove a key-value pair from the hash map.
+//         // T.C :) O(1);
+//         map.remove("Anu");
+//         System.out.println(map);
+//     }
+// }
+
+
+// Iterations on HashMap:)
+
 import java.util.*;
-public class Hash1 {
+public class Hash1{
     public static void main(String[] args){
-        HashMap<String, Integer > map = new HashMap<>();// Creation of HashMap
-        // Insertion of key-value pairs :) put() method
-        // T.C :) O(1) on average case and O(n) in worst case.
-        map.put("Ayush",1);
-        map.put("Anukriti",2);
-        map.put("Lakhan",3);
-        map.put("Anu",4);
+        HashMap<String , Integer> map = new HashMap<>();
+        map.put("India",100);
+        map.put("USA",200);
+        map.put("China",300);
+        map.put("Nepal",400);
+        map.put("Bhutan",500);
         System.out.println(map);
-
-        // get() - To retrieve the value associated with a specific key.
-        // T.C :) (1);
-        int data = map.get("Ayush");
-        System.out.println(data);
-
-        // containsKey() - To check if a specific key exists in the hash map.
-        // T.C :) O(1);
-        boolean check = map.containsKey("Anukriti");
-        System.out.println(check);
-
-        // remove() - To remove a key-value pair from the hash map.
-        // T.C :) O(1);
-        map.remove("Anu");
-        System.out.println(map);
+        // Iterating on HashMap
+        // T.C :) O(n) in worst case
+     Set<String>keys = map.keySet();
+     System.out.println(keys);
+     for (String s : keys) {
+        System.out.println("key" + s + "value= "+ map.get(s));
+     }
     }
 }
