e98315e4425bc9e753949f74ac01af350bc43786
Ayush Singh <ayush.singh.csbs22@ggits.net>
2025-05-11 03:41:43 +0530

    feat: added DFS Implementation Code

--- Graph1.java
+++ Graph1.java
@@ -48,6 +48,79 @@
 
 //   BFS
 
+// import java.util.*;
+
+// public class Graph1 {
+
+//   static class Edge {
+//     int src;
+//     int dest;
+//     int wt;
+
+//     public Edge(int s, int d, int w) {
+//       this.src = s;
+//       this.dest = d;
+//       this.wt = w;
+//     }
+//   }
+
+//   static void createGraph(ArrayList<Edge>[] graph) {
+//     for (int i = 0; i < graph.length; i++) {
+//       graph[i] = new ArrayList<>();
+//     }
+
+//     // 0 :-
+//     graph[0].add(new Edge(0, 1, 5));
+
+//     // 1:-
+//     graph[1].add(new Edge(1, 0, 5));
+//     graph[1].add(new Edge(1, 2, 1));
+//     graph[1].add(new Edge(1, 3, 3));
+
+//     // 2:-
+//     graph[2].add(new Edge(2, 1, 1));
+//     graph[2].add(new Edge(2, 3, 1));
+//     graph[2].add(new Edge(2, 4, 2));
+
+//     // 3:-
+//     graph[3].add(new Edge(3, 1, 3));
+//     graph[3].add(new Edge(3, 2, 1));
+
+//     // 4:-
+//     graph[4].add(new Edge(4, 2, 2));
+//   }
+
+//   public static void bfs(ArrayList<Edge>[] graph) {
+//     Queue<Integer> q = new LinkedList<>();
+//     boolean vis[] = new boolean[graph.length];
+//     q.add(0);
+
+//     while (!q.isEmpty()) {
+//       int curr = q.remove();
+//       if (!vis[curr]) {
+//         System.out.println(curr);
+//         vis[curr] = true;
+
+//         for (Edge e : graph[curr]) {
+//           q.add(e.dest);
+//         }
+//       }
+//     }
+//   }
+
+//   public static void main(String[] args) {
+//     int v = 5;
+//     ArrayList<Edge>[] graph = new ArrayList[v]; // null -> empty ArrayList
+//     createGraph(graph);
+//     System.out.println("BFS Traversal:");
+//     bfs(graph);
+//   }
+// }
+
+
+
+// DFS:
+
 import java.util.*;
 
 public class Graph1 {
@@ -90,29 +163,27 @@ public class Graph1 {
     graph[4].add(new Edge(4, 2, 2));
   }
 
-  public static void bfs(ArrayList<Edge>[] graph) {
-    Queue<Integer> q = new LinkedList<>();
-    boolean vis[] = new boolean[graph.length];
-    q.add(0);
+public static void dfs(ArrayList<Edge>[] graph, int curr , boolean vis[]) {
+    System.out.println(curr);
+    vis[curr] = true;
 
-    while (!q.isEmpty()) {
-      int curr = q.remove();
-      if (!vis[curr]) {
-        System.out.println(curr);
-        vis[curr] = true;
-
-        for (Edge e : graph[curr]) {
-          q.add(e.dest);
-        }
+    for (Edge e : graph[curr]) {
+      if (!vis[e.dest]) {
+        dfs(graph, e.dest, vis);
       }
     }
   }
 
+  public static void dfs(ArrayList<Edge>[] graph) {
+    boolean vis[] = new boolean[graph.length];
+    dfs(graph, 0, vis);
+
+}
   public static void main(String[] args) {
     int v = 5;
     ArrayList<Edge>[] graph = new ArrayList[v]; // null -> empty ArrayList
     createGraph(graph);
-    System.out.println("BFS Traversal:");
-    bfs(graph);
+    System.out.println("DFS Traversal:");
+    dfs(graph);
   }
 }
