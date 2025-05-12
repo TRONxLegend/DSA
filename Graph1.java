f2eaa57bf0aefa28103ab0d8f78dfbc67c6762a1
Ayush Singh <ayush.singh.csbs22@ggits.net>
2025-05-12 16:39:52 +0530

    feat:has Path code

--- Graph1.java
+++ Graph1.java
@@ -121,10 +121,80 @@
 
 // DFS:
 
+// import java.util.*;
+
+// public class Graph1 {
+
+//   static class Edge {
+//     int src;
+//     int dest;
+//     int wt;
+
+//     public Edge(int s, int d, int w) {
+//       this.src = s;
+//       this.dest = d;
+//       this.wt = w;
+//     }
+//   }
+
+//   static void createGraph(ArrayList<Edge>[] graph) {
+//     for (int i = 0; i < graph.length; i++) {
+//       graph[i] = new ArrayList<>();
+//     }
+
+//     // 0 :-
+//     graph[0].add(new Edge(0, 1, 5));
+
+//     // 1:-
+//     graph[1].add(new Edge(1, 0, 5));
+//     graph[1].add(new Edge(1, 2, 1));
+//     graph[1].add(new Edge(1, 3, 3));
+
+//     // 2:-
+//     graph[2].add(new Edge(2, 1, 1));
+//     graph[2].add(new Edge(2, 3, 1));
+//     graph[2].add(new Edge(2, 4, 2));
+
+//     // 3:-
+//     graph[3].add(new Edge(3, 1, 3));
+//     graph[3].add(new Edge(3, 2, 1));
+
+//     // 4:-
+//     graph[4].add(new Edge(4, 2, 2));
+//   }
+
+// public static void dfs(ArrayList<Edge>[] graph, int curr , boolean vis[]) {
+//     System.out.println(curr);
+//     vis[curr] = true;
+
+//     for (Edge e : graph[curr]) {
+//       if (!vis[e.dest]) {
+//         dfs(graph, e.dest, vis);
+//       }
+//     }
+//   }
+
+//   public static void dfs(ArrayList<Edge>[] graph) {
+//     boolean vis[] = new boolean[graph.length];
+//     dfs(graph, 0, vis);
+
+// }
+//   public static void main(String[] args) {
+//     int v = 5;
+//     ArrayList<Edge>[] graph = new ArrayList[v]; // null -> empty ArrayList
+//     createGraph(graph);
+//     System.out.println("DFS Traversal:");
+//     dfs(graph);
+//   }
+// }
+
+
+
+// Question 1: HAS PATH OR NOT:)    //
+
 import java.util.*;
 
 public class Graph1 {
-
   static class Edge {
     int src;
     int dest;
@@ -137,53 +207,62 @@ public class Graph1 {
     }
   }
 
-  static void createGraph(ArrayList<Edge>[] graph) {
+  public static void createGraph(ArrayList<Edge>[] graph) {
     for (int i = 0; i < graph.length; i++) {
       graph[i] = new ArrayList<>();
     }
 
-    // 0 :-
     graph[0].add(new Edge(0, 1, 5));
 
-    // 1:-
     graph[1].add(new Edge(1, 0, 5));
     graph[1].add(new Edge(1, 2, 1));
     graph[1].add(new Edge(1, 3, 3));
 
-    // 2:-
     graph[2].add(new Edge(2, 1, 1));
     graph[2].add(new Edge(2, 3, 1));
     graph[2].add(new Edge(2, 4, 2));
 
-    // 3:-
     graph[3].add(new Edge(3, 1, 3));
     graph[3].add(new Edge(3, 2, 1));
 
-    // 4:-
     graph[4].add(new Edge(4, 2, 2));
   }
 
-public static void dfs(ArrayList<Edge>[] graph, int curr , boolean vis[]) {
-    System.out.println(curr);
-    vis[curr] = true;
+  public static boolean hasPath(ArrayList<Edge>[] graph, int src, int dest, boolean[] vis, List<Integer> path) {
+    path.add(src);
+    if (src == dest) {
+      return true;
+    }
 
-    for (Edge e : graph[curr]) {
+    vis[src] = true;
+
+    for (int i = 0; i < graph[src].size(); i++) {
+      Edge e = graph[src].get(i);
       if (!vis[e.dest]) {
-        dfs(graph, e.dest, vis);
+        if (hasPath(graph, e.dest, dest, vis, path)) {
+          return true;
+        }
       }
     }
+
+    path.remove(path.size() - 1); // Backtrack
+    return false;
   }
 
-  public static void dfs(ArrayList<Edge>[] graph) {
-    boolean vis[] = new boolean[graph.length];
-    dfs(graph, 0, vis);
-
-}
   public static void main(String[] args) {
     int v = 5;
-    ArrayList<Edge>[] graph = new ArrayList[v]; // null -> empty ArrayList
+    ArrayList<Edge>[] graph = new ArrayList[v];
     createGraph(graph);
-    System.out.println("DFS Traversal:");
-    dfs(graph);
+
+    boolean[] vis = new boolean[v];
+    List<Integer> path = new ArrayList<>();
+
+    if ((()hasPath(graph, 0, 4
+        ), vis, path)) {
+      System.out.println("Path exists from 0 to 4");
+      System.out.println("Path: " + path);
+    } else {
+      System.out.println("No path exists from 0 to 4");
+    }
   }
 }
