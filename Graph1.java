1cd8a175457ad85244585c84eaf091b246daf52d
Ayush Singh <ayush.singh.csbs22@ggits.net>
2025-05-15 08:57:37 +0530

    feat : added code of Connected Components by BFS

--- Graph1.java
+++ Graph1.java
@@ -192,9 +192,90 @@
 
 // Question 1: HAS PATH OR NOT:)    //
 
+// import java.util.*;
+
+// public class Graph1 {
+//   static class Edge {
+//     int src;
+//     int dest;
+//     int wt;
+
+//     public Edge(int s, int d, int w) {
+//       this.src = s;
+//       this.dest = d;
+//       this.wt = w;
+//     }
+//   }
+
+//   public static void createGraph(ArrayList<Edge>[] graph) {
+//     for (int i = 0; i < graph.length; i++) {
+//       graph[i] = new ArrayList<>();
+//     }
+
+//     graph[0].add(new Edge(0, 1, 5));
+
+//     graph[1].add(new Edge(1, 0, 5));
+//     graph[1].add(new Edge(1, 2, 1));
+//     graph[1].add(new Edge(1, 3, 3));
+
+//     graph[2].add(new Edge(2, 1, 1));
+//     graph[2].add(new Edge(2, 3, 1));
+//     graph[2].add(new Edge(2, 4, 2));
+
+//     graph[3].add(new Edge(3, 1, 3));
+//     graph[3].add(new Edge(3, 2, 1));
+
+//     graph[4].add(new Edge(4, 2, 2));
+//   }
+
+//   public static boolean hasPath(ArrayList<Edge>[] graph, int src, int dest, boolean[] vis, List<Integer> path) {
+//     path.add(src);
+//     if (src == dest) {
+//       return true;
+//     }
+
+//     vis[src] = true;
+
+//     for (int i = 0; i < graph[src].size(); i++) {
+//       Edge e = graph[src].get(i);
+//       if (!vis[e.dest]) {
+//         if (hasPath(graph, e.dest, dest, vis, path)) {
+//           return true;
+//         }
+//       }
+//     }
+
+//     path.remove(path.size() - 1); // Backtrack
+//     return false;
+//   }
+
+//   public static void main(String[] args) {
+//     int v = 5;
+//     ArrayList<Edge>[] graph = new ArrayList[v];
+//     createGraph(graph);
+
+//     boolean[] vis = new boolean[v];
+//     List<Integer> path = new ArrayList<>();
+
+//     if ((()hasPath(graph, 0, 4
+//         ), vis, path)) {
+//       System.out.println("Path exists from 0 to 4");
+//       System.out.println("Path: " + path);
+//     } else {
+//       System.out.println("No path exists from 0 to 4");
+//     }
+//   }
+// }
+
+
+
+
+// Connected Components
+
 import java.util.*;
 
 public class Graph1 {
+
   static class Edge {
     int src;
     int dest;
@@ -207,46 +288,52 @@ public class Graph1 {
     }
   }
 
-  public static void createGraph(ArrayList<Edge>[] graph) {
+  static void createGraph(ArrayList<Edge>[] graph) {
     for (int i = 0; i < graph.length; i++) {
       graph[i] = new ArrayList<>();
     }
 
+    // Component 1
     graph[0].add(new Edge(0, 1, 5));
-
     graph[1].add(new Edge(1, 0, 5));
     graph[1].add(new Edge(1, 2, 1));
-    graph[1].add(new Edge(1, 3, 3));
-
     graph[2].add(new Edge(2, 1, 1));
-    graph[2].add(new Edge(2, 3, 1));
-    graph[2].add(new Edge(2, 4, 2));
 
-    graph[3].add(new Edge(3, 1, 3));
-    graph[3].add(new Edge(3, 2, 1));
-
-    graph[4].add(new Edge(4, 2, 2));
+    // Component 2
+    graph[3].add(new Edge(3, 4, 1));
+    graph[4].add(new Edge(4, 3, 1));
   }
 
-  public static boolean hasPath(ArrayList<Edge>[] graph, int src, int dest, boolean[] vis, List<Integer> path) {
-    path.add(src);
-    if (src == dest) {
-      return true;
+  public static void bfs(ArrayList<Edge>[] graph) {
+    boolean[] vis = new boolean[graph.length];
+    int count = 0;
+
+    for (int i = 0; i < graph.length; i++) {
+      if (!vis[i]) {
+        System.out.println("Connected Component " + (++count) + ":");
+        bfsutil(graph, vis, i); // ✅ FIXED: pass i
+        System.out.println();
+      }
     }
+    System.out.println("Total Connected Components: " + count);
+  }
 
-    vis[src] = true;
+  public static void bfsutil(ArrayList<Edge>[] graph, boolean[] vis, int start) {
+    Queue<Integer> q = new LinkedList<>();
+    q.add(start);
+    vis[start] = true;
 
-    for (int i = 0; i < graph[src].size(); i++) {
-      Edge e = graph[src].get(i);
-      if (!vis[e.dest]) {
-        if (hasPath(graph, e.dest, dest, vis, path)) {
-          return true;
+    while (!q.isEmpty()) {
+      int curr = q.poll();
+      System.out.print(curr + " ");
+
+      for (Edge e : graph[curr]) {
+        if (!vis[e.dest]) {
+          q.add(e.dest);
+          vis[e.dest] = true;
         }
       }
     }
-
-    path.remove(path.size() - 1); // Backtrack
-    return false;
   }
 
   public static void main(String[] args) {
@@ -254,15 +341,7 @@ public class Graph1 {
     ArrayList<Edge>[] graph = new ArrayList[v];
     createGraph(graph);
 
-    boolean[] vis = new boolean[v];
-    List<Integer> path = new ArrayList<>();
-
-    if ((()hasPath(graph, 0, 4
-        ), vis, path)) {
-      System.out.println("Path exists from 0 to 4");
-      System.out.println("Path: " + path);
-    } else {
-      System.out.println("No path exists from 0 to 4");
-    }
+    System.out.println("BFS Traversal and Connected Components:");
+    bfs(graph);
   }
 }
