df994ef1be7def7d6044d2266a51b498cb030ab2
Ayush Singh <ayush.singh.csbs22@ggits.net>
2025-05-17 23:57:34 +0530

    feat: Added Code of All Paths From Source To Destination

--- Graph1.java
+++ Graph1.java
@@ -563,6 +563,76 @@
 
 // TopologicalSort Using BFS (Kahn's Algorithm): //
 
+// import java.util.*;
+// public class Graph1{
+//     public static class Edge{
+//         int src;
+//         int dest;
+//         public Edge(int s , int d){
+//        this.src = s;
+//        this.dest = d;
+//         }
+//     }
+//     public static void createGraph(ArrayList<Edge> [] graph){
+//         for(int i = 0 ; i<graph.length; i++){
+//             graph[i] = new ArrayList<>();
+//         }
+//         graph[2].add(new Edge(2,3));
+
+//         graph[3].add(new Edge(3,1));
+
+//         graph[4].add(new Edge(4,0));
+//         graph[4].add(new Edge(4,1));
+
+//         graph[5].add(new Edge(5,0));
+//         graph[5].add(new Edge(5,2));
+//     }  
+//    public static void calcIndeg(ArrayList<Edge>[] graph, int indeg[]){
+//     for(int i = 0 ; i < graph.length; i++){
+//         int v =i;
+//         for(int j = 0 ; j<graph[v].size(); j++){
+//             Edge e = graph[v].get(j);
+//             indeg[e.dest]++;
+//         }
+//     }
+//    }
+//    public static void topSort(ArrayList<Edge>[] graph){
+//     int indeg[] = new int[graph.length];
+//     calcIndeg(graph, indeg);
+//     Queue<Integer> q = new LinkedList<>();
+//     for(int i = 0; i<indeg.length; i++){
+//         if(indeg[i] == 0){
+//             q.add(i);
+//         }
+//     }
+//     while(!q.isEmpty()){
+//         int curr = q.remove();
+//         System.out.print(curr +" ");
+
+//         for(int i = 0 ; i< graph[curr].size(); i++){
+//             Edge e = graph[curr].get(i);
+//             indeg[e.dest]--;
+//             if(indeg[e.dest] == 0){
+//                 q.add(e.dest);
+//             }
+
+//         }
+//     }
+//     System.out.println();
+//    }
+//     public static void main(String[] args){
+//         int V = 6;
+//      ArrayList<Edge> [] graph = new ArrayList[V];
+//     createGraph(graph);
+//     System.out.println("Topological Sort using BFS:");
+//     topSort(graph);
+//         }
+// }
+
+
+
+// All Paths From Source to Destination:  //
+
 import java.util.*;
 public class Graph1{
     public static class Edge{
@@ -587,44 +657,23 @@ public class Graph1{
         graph[5].add(new Edge(5,0));
         graph[5].add(new Edge(5,2));
     }  
-   public static void calcIndeg(ArrayList<Edge>[] graph, int indeg[]){
-    for(int i = 0 ; i < graph.length; i++){
-        int v =i;
-        for(int j = 0 ; j<graph[v].size(); j++){
-            Edge e = graph[v].get(j);
-            indeg[e.dest]++;
-        }
+public static void printallPath(ArrayList<Edge>[] graph , int curr, int dest , String path){
+    if(curr == dest){
+        System.out.println(path+dest);
+        return;
     }
-   }
-   public static void topSort(ArrayList<Edge>[] graph){
-    int indeg[] = new int[graph.length];
-    calcIndeg(graph, indeg);
-    Queue<Integer> q = new LinkedList<>();
-    for(int i = 0; i<indeg.length; i++){
-        if(indeg[i] == 0){
-            q.add(i);
-        }
+    for(int i =0; i<graph[curr].size();i++){
+        Edge e = graph[curr].get(i);
+        printallPath(graph, e.dest, dest, path+curr);
     }
-    while(!q.isEmpty()){
-        int curr = q.remove();
-        System.out.print(curr +" ");
-
-        for(int i = 0 ; i< graph[curr].size(); i++){
-            Edge e = graph[curr].get(i);
-            indeg[e.dest]--;
-            if(indeg[e.dest] == 0){
-                q.add(e.dest);
-            }
-
-        }
-    }
-    System.out.println();
-   }
-    public static void main(String[] args){
-        int V = 6;
-     ArrayList<Edge> [] graph = new ArrayList[V];
+}
+public static void main(String[] args){
+    int V = 6;
+    ArrayList<Edge>[] graph = new ArrayList[V];
     createGraph(graph);
-    System.out.println("Topological Sort using BFS:");
-    topSort(graph);
-        }
+    int src = 5;
+    int dest =1;
+    System.out.println("All Paths from " + src + " to " + dest + ":");
+    printallPath(graph, src, dest, "");
+}
 }
