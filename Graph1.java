2f81f6ba15426bc8a3fcf2f20fe6cf408e0ae56f
Ayush Singh <ayush.singh.csbs22@ggits.net>
2025-05-17 23:40:17 +0530

    feat: Topological Sort using kahn's Algorithm

--- Graph1.java
+++ Graph1.java
@@ -503,18 +503,78 @@
 
 //   Topological Sort:-
 
+// import java.util.*;
+// public class Graph1{
+//     static class Edge{
+//         int src;
+//         int dest;
+//     public Edge(int s,int d){
+//         this.src = s;
+//         this.dest = d;
+//     }
+//     }
+//     public static void CreateGraph(ArrayList<Edge>[] graph){
+//         for(int i = 0; i< graph.length; i++){
+//             graph[i] = new ArrayList<>();
+//         }
+//         graph[2].add(new Edge(2,3));
+
+//         graph[3].add(new Edge(3,1));
+
+//         graph[4].add(new Edge(4,0));
+//         graph[4].add(new Edge(4,1));
+
+//         graph[5].add(new Edge(5,0));
+//         graph[5].add(new Edge(5,2));
+//     }
+//     public static void TopologicalSort(ArrayList<Edge>[] graph){
+//         boolean vis[] = new boolean[graph.length];
+//         Stack<Integer> st = new Stack<>();
+//         for(int i = 0 ; i< graph.length;i++){
+//             if(!vis[i]){
+//                 TopologicalSortUtil(graph, i, vis, st);
+//             }
+//         }
+//         while(!st.isEmpty()){
+//             System.out.print(st.pop() + " ");
+//             }
+//         }
+// public static void  TopologicalSortUtil(ArrayList<Edge>[] graph, int curr, boolean vis[], Stack<Integer> st){
+//     vis[curr] = true;
+//     for(int i = 0; i < graph[curr].size(); i++){
+//         Edge e = graph[curr].get(i);
+//         if(!vis[e.dest]){
+//             TopologicalSortUtil(graph, e.dest, vis, st);
+//         }
+//     }
+//     st.push(curr);
+// }
+//     public static void main(String[] args){
+//         int V= 6;
+//         ArrayList<Edge>[] graph = new ArrayList[V];
+//         CreateGraph(graph);
+//         System.out.println("Topological Sort:");
+//         TopologicalSort(graph);
+//         System.out.println();
+        
+//     }
+// }
+
+
+// TopologicalSort Using BFS (Kahn's Algorithm): //
+
 import java.util.*;
 public class Graph1{
-    static class Edge{
+    public static class Edge{
         int src;
         int dest;
-    public Edge(int s,int d){
-        this.src = s;
-        this.dest = d;
+        public Edge(int s , int d){
+       this.src = s;
+       this.dest = d;
+        }
     }
-    }
-    public static void CreateGraph(ArrayList<Edge>[] graph){
-        for(int i = 0; i< graph.length; i++){
+    public static void createGraph(ArrayList<Edge> [] graph){
+        for(int i = 0 ; i<graph.length; i++){
             graph[i] = new ArrayList<>();
         }
         graph[2].add(new Edge(2,3));
@@ -526,36 +586,45 @@ public class Graph1{
 
         graph[5].add(new Edge(5,0));
         graph[5].add(new Edge(5,2));
-    }
-    public static void TopologicalSort(ArrayList<Edge>[] graph){
-        boolean vis[] = new boolean[graph.length];
-        Stack<Integer> st = new Stack<>();
-        for(int i = 0 ; i< graph.length;i++){
-            if(!vis[i]){
-                TopologicalSortUtil(graph, i, vis, st);
-            }
-        }
-        while(!st.isEmpty()){
-            System.out.print(st.pop() + " ");
-            }
-        }
-public static void  TopologicalSortUtil(ArrayList<Edge>[] graph, int curr, boolean vis[], Stack<Integer> st){
-    vis[curr] = true;
-    for(int i = 0; i < graph[curr].size(); i++){
-        Edge e = graph[curr].get(i);
-        if(!vis[e.dest]){
-            TopologicalSortUtil(graph, e.dest, vis, st);
+    }  
+   public static void calcIndeg(ArrayList<Edge>[] graph, int indeg[]){
+    for(int i = 0 ; i < graph.length; i++){
+        int v =i;
+        for(int j = 0 ; j<graph[v].size(); j++){
+            Edge e = graph[v].get(j);
+            indeg[e.dest]++;
         }
     }
-    st.push(curr);
-}
+   }
+   public static void topSort(ArrayList<Edge>[] graph){
+    int indeg[] = new int[graph.length];
+    calcIndeg(graph, indeg);
+    Queue<Integer> q = new LinkedList<>();
+    for(int i = 0; i<indeg.length; i++){
+        if(indeg[i] == 0){
+            q.add(i);
+        }
+    }
+    while(!q.isEmpty()){
+        int curr = q.remove();
+        System.out.print(curr +" ");
+
+        for(int i = 0 ; i< graph[curr].size(); i++){
+            Edge e = graph[curr].get(i);
+            indeg[e.dest]--;
+            if(indeg[e.dest] == 0){
+                q.add(e.dest);
+            }
+
+        }
+    }
+    System.out.println();
+   }
     public static void main(String[] args){
-        int V= 6;
-        ArrayList<Edge>[] graph = new ArrayList[V];
-        CreateGraph(graph);
-        System.out.println("Topological Sort:");
-        TopologicalSort(graph);
-        System.out.println();
-        
-    }
+        int V = 6;
+     ArrayList<Edge> [] graph = new ArrayList[V];
+    createGraph(graph);
+    System.out.println("Topological Sort using BFS:");
+    topSort(graph);
+        }
 }
