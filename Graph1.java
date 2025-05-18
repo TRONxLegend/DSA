c11fce25b4057fdc43637514ae120c51c46c24e4
Ayush Singh <ayush.singh.csbs22@ggits.net>
2025-05-18 23:52:31 +0530

    feat: Added Code of Prim's Algorithm

--- Graph1.java
+++ Graph1.java
@@ -767,65 +767,139 @@
 
 
 // Bellman-Ford Algorithm: Shortest Path in a Weighted Graph with Negative Weights //
+// import java.util.*;
+// public class Graph1{
+//     public static class Edge{
+//         int src;
+//         int dest;
+//         int wt;
+//         public Edge(int s , int d , int w){
+//             this.src = s;
+//             this.dest = d;
+//             this.wt = w;
+//         }
+//     }
+//     public static void createGraph(ArrayList<Edge>[] graph){
+//         for(int i = 0; i<graph.length; i++){
+//             graph[i] = new ArrayList<>();
+//         }
+//         graph[0].add(new Edge(0, 1, 2));
+//         graph[0].add(new Edge(0, 2, 4));
+    
+//         graph[1].add(new Edge(1, 2, -4));
+        
+//         graph[2].add(new Edge(2,3,2));
+
+//         graph[3].add(new Edge(3,4,4));
+
+//         graph[4].add(new Edge(4,1,-1));
+      
+//     }
+//     public static void bellamnFord(ArrayList<Edge>[] graph,int src){
+//         int dist[] = new int[graph.length];
+//         for(int i = 0; i< dist.length; i++){
+//             if(i != src){
+//                 dist[i] = Integer.MAX_VALUE;
+//             }
+//         }
+//         int V = graph.length;
+//         for(int i = 0; i<V-1; i++){
+//             for(int j =0 ; j<graph.length; j++){
+//                 for(int k = 0;k<graph[j].size();k++){
+//                     Edge e = graph[j].get(k);
+//                     int u =e.src;
+//                     int v = e.dest;
+//                     int wt = e.wt;
+//                     if(dist[u] != Integer.MAX_VALUE && dist[u] + wt < dist[v]){
+//                         dist[v] = dist[u] + wt;
+//                     }
+//                 }
+//             }
+//         }
+//         for(int i = 0; i< dist.length; i++){
+//             System.out.println( dist[i] + " ");
+//         }
+//         System.out.println();
+//     }
+//     public static void main(String[] args){
+//         int V = 5;
+//         ArrayList<Edge>[] graph = new ArrayList[V];
+//         createGraph(graph);
+//         int src = 0;
+//         bellamnFord(graph, src);
+//     }
+// }
+
+
+// PRIM's Algorithm: Minimum Spanning Tree (MST) //
+
 import java.util.*;
 public class Graph1{
-    public static class Edge{
+    static class Edge{
         int src;
         int dest;
         int wt;
-        public Edge(int s , int d , int w){
+        public Edge(int s,int d,int w){
             this.src = s;
             this.dest = d;
             this.wt = w;
         }
     }
     public static void createGraph(ArrayList<Edge>[] graph){
-        for(int i = 0; i<graph.length; i++){
+        for(int i=0; i<graph.length; i++){
             graph[i] = new ArrayList<>();
         }
-        graph[0].add(new Edge(0, 1, 2));
-        graph[0].add(new Edge(0, 2, 4));
-    
-        graph[1].add(new Edge(1, 2, -4));
-        
-        graph[2].add(new Edge(2,3,2));
+        graph[0].add(new Edge(0,1,10));
+        graph[0].add(new Edge(0,2,15));
+        graph[0].add(new Edge(0,3,30));
 
-        graph[3].add(new Edge(3,4,4));
+        graph[1].add(new Edge(1,0,10));
+        graph[1].add(new Edge(1,3,40));
 
-        graph[4].add(new Edge(4,1,-1));
-      
+        graph[2].add(new Edge(2,0,15));
+        graph[2].add(new Edge(2,3,50));
+
+        graph[3].add(new Edge(3,1,40));
+        graph[3].add(new Edge(3,2,50));
     }
-    public static void bellamnFord(ArrayList<Edge>[] graph,int src){
-        int dist[] = new int[graph.length];
-        for(int i = 0; i< dist.length; i++){
-            if(i != src){
-                dist[i] = Integer.MAX_VALUE;
-            }
+    static class Pair implements Comparable<Pair>{
+        int n;
+        int cost;
+        public Pair(int n, int c){
+            this.n = n;
+            this.cost = c;
         }
-        int V = graph.length;
-        for(int i = 0; i<V-1; i++){
-            for(int j =0 ; j<graph.length; j++){
-                for(int k = 0;k<graph[j].size();k++){
-                    Edge e = graph[j].get(k);
-                    int u =e.src;
-                    int v = e.dest;
-                    int wt = e.wt;
-                    if(dist[u] != Integer.MAX_VALUE && dist[u] + wt < dist[v]){
-                        dist[v] = dist[u] + wt;
-                    }
+        @Override
+        public int compareTo(Pair p2){
+            return this.cost - p2.cost; // Ascending order
+        }
+    }
+    public static void PrimsMST(ArrayList<Edge>[] graph){
+        boolean vis[] = new boolean[graph.length];
+        PriorityQueue<Pair> pq = new PriorityQueue<>();
+        pq.add(new Pair(0,0)); // Starting from node 0 with cost 0
+        int cost = 0;  
+        while(!pq.isEmpty()){
+            Pair curr = pq.remove();
+            if(vis[curr.n]){
+                continue;
+            }
+            vis[curr.n] = true;
+            cost += curr.cost;
+            for(int i=0; i<graph[curr.n].size(); i++){
+                Edge e = graph[curr.n].get(i);
+                if(!vis[e.dest]){
+                    pq.add(new Pair(e.dest, e.wt));
                 }
             }
         }
-        for(int i = 0; i< dist.length; i++){
-            System.out.println( dist[i] + " ");
-        }
-        System.out.println();
+        System.out.println(cost);
     }
     public static void main(String[] args){
-        int V = 5;
+        int V = 4;
         ArrayList<Edge>[] graph = new ArrayList[V];
         createGraph(graph);
-        int src = 0;
-        bellamnFord(graph, src);
+       PrimsMST(graph);
+
     }
 }
