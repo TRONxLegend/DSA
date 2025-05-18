36ea728772c122938597e42fd19b7b1ccae1c0a7
Ayush Singh <ayush.singh.csbs22@ggits.net>
2025-05-18 16:17:56 +0530

    feat: Added Code of Bellman Ford Algorithm

--- Graph1.java
+++ Graph1.java
@@ -681,6 +681,92 @@
 
 // Dijkstra's Algorithm: Shortest Path in a Weighted Graph //
 
+// import java.util.*;
+// public class Graph1{
+//     public static class Edge{
+//         int src;
+//         int dest;
+//         int wt;
+//         public Edge(int s , int d , int w){
+//             this.src = s;
+//             this.dest = d;
+//             this.wt = w;
+//         }
+//     }
+//     public static void createGraph(ArrayList<Edge>[] graph){
+//         for(int i = 0; i<graph.length; i++){
+//             graph[i] = new ArrayList<>();
+//         }
+//         graph[0].add(new Edge(0, 1, 2));
+//         graph[0].add(new Edge(0, 2, 4));
+    
+//         graph[1].add(new Edge(1, 3 , 7));
+//         graph[1].add(new Edge(1, 2, 1));
+        
+//         graph[2].add(new Edge(2,4,3));
+
+//         graph[3].add(new Edge(3,5,1));
+
+//         graph[4].add(new Edge(4,3,2));
+//         graph[4].add(new Edge(4,5,5));
+//     }
+//     static class Pair implements Comparable<Pair>{
+//         int n;
+//         int path;
+//         public Pair(int n, int path){
+//             this.n = n;
+//             this.path =path;
+//         }
+//         @Override
+//         public int compareTo(Pair p2){
+//             return this.path - p2.path; // Ascending order
+//         }
+//     }
+//     public static void dijkstra(ArrayList<Edge>[] graph, int src){
+//    int dist[] =new int[graph.length];
+//    for(int i = 0 ; i<graph.length; i++){
+//     if(i != src){
+//         dist[i] = Integer.MAX_VALUE;
+//     }
+//    } 
+//     boolean vis[] = new boolean[graph.length];
+//    PriorityQueue<Pair> pq =new PriorityQueue<>();
+//    pq.add(new Pair(src,0));
+//    while(!pq.isEmpty()){
+//     Pair curr = pq.remove();
+//     if(!vis[curr.n]){
+//         vis[curr.n] = true;
+
+//         for(int i =0 ; i<graph[curr.n].size(); i++){
+//             Edge e = graph[curr.n].get(i);
+//             int u = e.src;
+//             int v= e.dest;
+//             int wt = e.wt;
+//             if(dist[u] + wt < dist[v]){
+//                 dist[v] = dist[u]+wt;
+//                 pq.add(new Pair(v, dist[v]));
+//             }
+//         }
+//     }
+//    }  
+//     for(int i = 0; i<dist.length; i++){
+//         System.out.println("Distance from " + src + " to " + i + " is: " + dist[i] + " ");
+//     }
+//     System.out.println();
+//     }
+//     public static void main(String[] args){
+//         int V = 6;
+//         ArrayList<Edge>[] graph = new ArrayList[V];
+//         createGraph(graph);
+//         int src = 0;
+//         int dest = 5;
+//         dijkstra(graph, src);
+//     }
+// }
+
+
+
+// Bellman-Ford Algorithm: Shortest Path in a Weighted Graph with Negative Weights //
 import java.util.*;
 public class Graph1{
     public static class Edge{
@@ -700,66 +786,46 @@ public class Graph1{
         graph[0].add(new Edge(0, 1, 2));
         graph[0].add(new Edge(0, 2, 4));
     
-        graph[1].add(new Edge(1, 3 , 7));
-        graph[1].add(new Edge(1, 2, 1));
+        graph[1].add(new Edge(1, 2, -4));
         
-        graph[2].add(new Edge(2,4,3));
+        graph[2].add(new Edge(2,3,2));
 
-        graph[3].add(new Edge(3,5,1));
+        graph[3].add(new Edge(3,4,4));
 
-        graph[4].add(new Edge(4,3,2));
-        graph[4].add(new Edge(4,5,5));
+        graph[4].add(new Edge(4,1,-1));
+      
     }
-    static class Pair implements Comparable<Pair>{
-        int n;
-        int path;
-        public Pair(int n, int path){
-            this.n = n;
-            this.path =path;
-        }
-        @Override
-        public int compareTo(Pair p2){
-            return this.path - p2.path; // Ascending order
-        }
-    }
-    public static void dijkstra(ArrayList<Edge>[] graph, int src){
-   int dist[] =new int[graph.length];
-   for(int i = 0 ; i<graph.length; i++){
-    if(i != src){
-        dist[i] = Integer.MAX_VALUE;
-    }
-   } 
-    boolean vis[] = new boolean[graph.length];
-   PriorityQueue<Pair> pq =new PriorityQueue<>();
-   pq.add(new Pair(src,0));
-   while(!pq.isEmpty()){
-    Pair curr = pq.remove();
-    if(!vis[curr.n]){
-        vis[curr.n] = true;
-
-        for(int i =0 ; i<graph[curr.n].size(); i++){
-            Edge e = graph[curr.n].get(i);
-            int u = e.src;
-            int v= e.dest;
-            int wt = e.wt;
-            if(dist[u] + wt < dist[v]){
-                dist[v] = dist[u]+wt;
-                pq.add(new Pair(v, dist[v]));
+    public static void bellamnFord(ArrayList<Edge>[] graph,int src){
+        int dist[] = new int[graph.length];
+        for(int i = 0; i< dist.length; i++){
+            if(i != src){
+                dist[i] = Integer.MAX_VALUE;
             }
         }
-    }
-   }  
-    for(int i = 0; i<dist.length; i++){
-        System.out.println("Distance from " + src + " to " + i + " is: " + dist[i] + " ");
-    }
-    System.out.println();
+        int V = graph.length;
+        for(int i = 0; i<V-1; i++){
+            for(int j =0 ; j<graph.length; j++){
+                for(int k = 0;k<graph[j].size();k++){
+                    Edge e = graph[j].get(k);
+                    int u =e.src;
+                    int v = e.dest;
+                    int wt = e.wt;
+                    if(dist[u] != Integer.MAX_VALUE && dist[u] + wt < dist[v]){
+                        dist[v] = dist[u] + wt;
+                    }
+                }
+            }
+        }
+        for(int i = 0; i< dist.length; i++){
+            System.out.println( dist[i] + " ");
+        }
+        System.out.println();
     }
     public static void main(String[] args){
-        int V = 6;
+        int V = 5;
         ArrayList<Edge>[] graph = new ArrayList[V];
         createGraph(graph);
         int src = 0;
-        int dest = 5;
-        dijkstra(graph, src);
+        bellamnFord(graph, src);
     }
 }
