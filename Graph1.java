5d05304560a5131c90907a09e8f88c818e3219eb
Ayush Singh <ayush.singh.csbs22@ggits.net>
2025-05-26 08:22:41 +0530

    feat : Added Solution of Cheapest Flights Within K Stops

--- Graph1.java
+++ Graph1.java
@@ -833,73 +833,156 @@
 
 // PRIM's Algorithm: Minimum Spanning Tree (MST) //
 
+// import java.util.*;
+// public class Graph1{
+//     static class Edge{
+//         int src;
+//         int dest;
+//         int wt;
+//         public Edge(int s,int d,int w){
+//             this.src = s;
+//             this.dest = d;
+//             this.wt = w;
+//         }
+//     }
+//     public static void createGraph(ArrayList<Edge>[] graph){
+//         for(int i=0; i<graph.length; i++){
+//             graph[i] = new ArrayList<>();
+//         }
+//         graph[0].add(new Edge(0,1,10));
+//         graph[0].add(new Edge(0,2,15));
+//         graph[0].add(new Edge(0,3,30));
+
+//         graph[1].add(new Edge(1,0,10));
+//         graph[1].add(new Edge(1,3,40));
+
+//         graph[2].add(new Edge(2,0,15));
+//         graph[2].add(new Edge(2,3,50));
+
+//         graph[3].add(new Edge(3,1,40));
+//         graph[3].add(new Edge(3,2,50));
+//     }
+//     static class Pair implements Comparable<Pair>{
+//         int n;
+//         int cost;
+//         public Pair(int n, int c){
+//             this.n = n;
+//             this.cost = c;
+//         }
+//         @Override
+//         public int compareTo(Pair p2){
+//             return this.cost - p2.cost; // Ascending order
+//         }
+//     }
+//     public static void PrimsMST(ArrayList<Edge>[] graph){
+//         boolean vis[] = new boolean[graph.length];
+//         PriorityQueue<Pair> pq = new PriorityQueue<>();
+//         pq.add(new Pair(0,0)); // Starting from node 0 with cost 0
+//         int cost = 0;  
+//         while(!pq.isEmpty()){
+//             Pair curr = pq.remove();
+//             if(vis[curr.n]){
+//                 continue;
+//             }
+//             vis[curr.n] = true;
+//             cost += curr.cost;
+//             for(int i=0; i<graph[curr.n].size(); i++){
+//                 Edge e = graph[curr.n].get(i);
+//                 if(!vis[e.dest]){
+//                     pq.add(new Pair(e.dest, e.wt));
+//                 }
+//             }
+//         }
+//         System.out.println(cost);
+//     }
+//     public static void main(String[] args){
+//         int V = 4;
+//         ArrayList<Edge>[] graph = new ArrayList[V];
+//         createGraph(graph);
+//        PrimsMST(graph);
+
+//     }
+// }
+
+
+
+// Qusetion: Cheapest Flights Within K Stops //
 import java.util.*;
 public class Graph1{
     static class Edge{
         int src;
         int dest;
         int wt;
-        public Edge(int s,int d,int w){
+        public Edge(int s, int d, int w){
             this.src = s;
             this.dest = d;
             this.wt = w;
         }
     }
-    public static void createGraph(ArrayList<Edge>[] graph){
-        for(int i=0; i<graph.length; i++){
+    public static void createGraph(int flights[][],ArrayList<Edge> graph[]){
+        for(int i =0 ;i<graph.length; i++){
             graph[i] = new ArrayList<>();
         }
-        graph[0].add(new Edge(0,1,10));
-        graph[0].add(new Edge(0,2,15));
-        graph[0].add(new Edge(0,3,30));
+        for(int i =0 ; i<flights.length; i++){
+            int src = flights[i][0];
+            int dest = flights[i][1];
+            int wt = flights[i][2];
+            graph[src].add(new Edge(src, dest, wt));
+        }
 
-        graph[1].add(new Edge(1,0,10));
-        graph[1].add(new Edge(1,3,40));
-
-        graph[2].add(new Edge(2,0,15));
-        graph[2].add(new Edge(2,3,50));
-
-        graph[3].add(new Edge(3,1,40));
-        graph[3].add(new Edge(3,2,50));
     }
-    static class Pair implements Comparable<Pair>{
-        int n;
+    static class Info{
+        int v; 
         int cost;
-        public Pair(int n, int c){
-            this.n = n;
-            this.cost = c;
-        }
-        @Override
-        public int compareTo(Pair p2){
-            return this.cost - p2.cost; // Ascending order
+        int stops;
+        public Info(int v, int c, int s){
+        this.v = v;
+        this.cost = c;
+        this.stops= s;
         }
     }
-    public static void PrimsMST(ArrayList<Edge>[] graph){
-        boolean vis[] = new boolean[graph.length];
-        PriorityQueue<Pair> pq = new PriorityQueue<>();
-        pq.add(new Pair(0,0)); // Starting from node 0 with cost 0
-        int cost = 0;  
-        while(!pq.isEmpty()){
-            Pair curr = pq.remove();
-            if(vis[curr.n]){
-                continue;
-            }
-            vis[curr.n] = true;
-            cost += curr.cost;
-            for(int i=0; i<graph[curr.n].size(); i++){
-                Edge e = graph[curr.n].get(i);
-                if(!vis[e.dest]){
-                    pq.add(new Pair(e.dest, e.wt));
-                }
+    public static int cheapestFlight(int n , int flights[][], int src ,int dest, int k){
+        ArrayList<Edge> graph[] = new ArrayList[n];
+        createGraph(flights, graph);
+        int dist[] = new int[n];
+        for(int i = 0; i<dist.length; i++){
+            if(i != src){
+                dist[i] = Integer.MAX_VALUE;
             }
         }
-        System.out.println(cost);
+       Queue<Info> q = new LinkedList<>();
+       q.add(new Info(src, 0, 0)); // Starting from source with cost 0 and 0 stops
+       while(!q.isEmpty()){
+        Info curr = q.remove();
+        if(curr.stops >k){
+            break;
+        }
+        for(int i =0 ; i<graph[curr.v].size(); i++){
+            Edge e = graph[curr.v].get(i);
+            int u =e.src;
+            int v= e.dest;
+            int wt = e.wt;
+            
+            if(dist[u] != Integer.MAX_VALUE && curr.cost + wt < dist[v] && curr.stops <= k){
+                dist[v] = curr.cost +wt;
+                q.add(new Info(v, dist[v], curr.stops + 1));
+            }
+        }
+       }
+       if(dist[dest] == Integer.MAX_VALUE){
+           return -1; // No path found
+       }
+         return dist[dest]; // Return the minimum cost to reach destination
     }
-    public static void main(String[] args){
-        int V = 4;
-        ArrayList<Edge>[] graph = new ArrayList[V];
-        createGraph(graph);
-       PrimsMST(graph);
-
+    public static void main(String args[]){
+        int n = 4; // Number of cities
+        int flights[][] ={{0,1,100},
+                          {1,2,100},
+                          {2,0,100},
+                          {1,3,600},
+                          {2,3,200}};
+        int src =0, dest =3, k=1; // Source city, destination city, and maximum stops allowed
+        cheapestFlight(n, flights, src, dest, k);
+        System.out.println("Cheapest flight cost from " + src + " to " + dest + " with at most " + k + " stops is: " + cheapestFlight(n, flights, src, dest, k));
     }
 }
