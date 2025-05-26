ac87e47638a562f4c1cbb9a971972b593aa059cf
Ayush Singh <ayush.singh.csbs22@ggits.net>
2025-05-26 09:09:35 +0530

    feat : Added Solution of Connecting Cities with Minimum Cost

--- Graph1.java
+++ Graph1.java
@@ -907,82 +907,132 @@
 
 
 // Qusetion: Cheapest Flights Within K Stops //
+// import java.util.*;
+// public class Graph1{
+//     static class Edge{
+//         int src;
+//         int dest;
+//         int wt;
+//         public Edge(int s, int d, int w){
+//             this.src = s;
+//             this.dest = d;
+//             this.wt = w;
+//         }
+//     }
+//     public static void createGraph(int flights[][],ArrayList<Edge> graph[]){
+//         for(int i =0 ;i<graph.length; i++){
+//             graph[i] = new ArrayList<>();
+//         }
+//         for(int i =0 ; i<flights.length; i++){
+//             int src = flights[i][0];
+//             int dest = flights[i][1];
+//             int wt = flights[i][2];
+//             graph[src].add(new Edge(src, dest, wt));
+//         }
+
+//     }
+//     static class Info{
+//         int v; 
+//         int cost;
+//         int stops;
+//         public Info(int v, int c, int s){
+//         this.v = v;
+//         this.cost = c;
+//         this.stops= s;
+//         }
+//     }
+//     public static int cheapestFlight(int n , int flights[][], int src ,int dest, int k){
+//         ArrayList<Edge> graph[] = new ArrayList[n];
+//         createGraph(flights, graph);
+//         int dist[] = new int[n];
+//         for(int i = 0; i<dist.length; i++){
+//             if(i != src){
+//                 dist[i] = Integer.MAX_VALUE;
+//             }
+//         }
+//        Queue<Info> q = new LinkedList<>();
+//        q.add(new Info(src, 0, 0)); // Starting from source with cost 0 and 0 stops
+//        while(!q.isEmpty()){
+//         Info curr = q.remove();
+//         if(curr.stops >k){
+//             break;
+//         }
+//         for(int i =0 ; i<graph[curr.v].size(); i++){
+//             Edge e = graph[curr.v].get(i);
+//             int u =e.src;
+//             int v= e.dest;
+//             int wt = e.wt;
+            
+//             if(dist[u] != Integer.MAX_VALUE && curr.cost + wt < dist[v] && curr.stops <= k){
+//                 dist[v] = curr.cost +wt;
+//                 q.add(new Info(v, dist[v], curr.stops + 1));
+//             }
+//         }
+//        }
+//        if(dist[dest] == Integer.MAX_VALUE){
+//            return -1; // No path found
+//        }
+//          return dist[dest]; // Return the minimum cost to reach destination
+//     }
+//     public static void main(String args[]){
+//         int n = 4; // Number of cities
+//         int flights[][] ={{0,1,100},
+//                           {1,2,100},
+//                           {2,0,100},
+//                           {1,3,600},
+//                           {2,3,200}};
+//         int src =0, dest =3, k=1; // Source city, destination city, and maximum stops allowed
+//         cheapestFlight(n, flights, src, dest, k);
+//         System.out.println("Cheapest flight cost from " + src + " to " + dest + " with at most " + k + " stops is: " + cheapestFlight(n, flights, src, dest, k));
+//     }
+// }
+
+
+// Question : Connecting Cities with Minimum Cost //
+
 import java.util.*;
 public class Graph1{
-    static class Edge{
-        int src;
+    static class Edge implements Comparable<Edge>{
         int dest;
-        int wt;
-        public Edge(int s, int d, int w){
-            this.src = s;
-            this.dest = d;
-            this.wt = w;
-        }
-    }
-    public static void createGraph(int flights[][],ArrayList<Edge> graph[]){
-        for(int i =0 ;i<graph.length; i++){
-            graph[i] = new ArrayList<>();
-        }
-        for(int i =0 ; i<flights.length; i++){
-            int src = flights[i][0];
-            int dest = flights[i][1];
-            int wt = flights[i][2];
-            graph[src].add(new Edge(src, dest, wt));
-        }
-
-    }
-    static class Info{
-        int v; 
         int cost;
-        int stops;
-        public Info(int v, int c, int s){
-        this.v = v;
-        this.cost = c;
-        this.stops= s;
+        public Edge(int d, int c){
+            this.dest = d;
+            this.cost = c;
         }
-    }
-    public static int cheapestFlight(int n , int flights[][], int src ,int dest, int k){
-        ArrayList<Edge> graph[] = new ArrayList[n];
-        createGraph(flights, graph);
-        int dist[] = new int[n];
-        for(int i = 0; i<dist.length; i++){
-            if(i != src){
-                dist[i] = Integer.MAX_VALUE;
+    
+        @Override
+        public int compareTo(Edge e2){
+            return this.cost - e2.cost; // Ascending order
+        }
+        }
+        public static int connectcities(int cities[][]){
+            PriorityQueue<Edge> pq = new PriorityQueue<>();
+            boolean vist[] = new boolean[cities.length];
+            int totalCost = 0;
+            pq.add(new Edge(0, 0)); // Start from city 0 with cost 0
+
+            while(!pq.isEmpty()){
+                Edge curr = pq.remove();
+                if(!vist[curr.dest]){
+                    vist[curr.dest] = true;
+                   totalCost += curr.cost;
+
+                   for(int i= 0; i<cities[curr.dest].length; i++){
+                    if(cities[curr.dest][i] != 0){
+                        pq.add(new Edge(i, cities[curr.dest][i]));
+                   }
+                }
             }
         }
-       Queue<Info> q = new LinkedList<>();
-       q.add(new Info(src, 0, 0)); // Starting from source with cost 0 and 0 stops
-       while(!q.isEmpty()){
-        Info curr = q.remove();
-        if(curr.stops >k){
-            break;
-        }
-        for(int i =0 ; i<graph[curr.v].size(); i++){
-            Edge e = graph[curr.v].get(i);
-            int u =e.src;
-            int v= e.dest;
-            int wt = e.wt;
-            
-            if(dist[u] != Integer.MAX_VALUE && curr.cost + wt < dist[v] && curr.stops <= k){
-                dist[v] = curr.cost +wt;
-                q.add(new Info(v, dist[v], curr.stops + 1));
-            }
-        }
-       }
-       if(dist[dest] == Integer.MAX_VALUE){
-           return -1; // No path found
-       }
-         return dist[dest]; // Return the minimum cost to reach destination
+        return totalCost;
     }
     public static void main(String args[]){
-        int n = 4; // Number of cities
-        int flights[][] ={{0,1,100},
-                          {1,2,100},
-                          {2,0,100},
-                          {1,3,600},
-                          {2,3,200}};
-        int src =0, dest =3, k=1; // Source city, destination city, and maximum stops allowed
-        cheapestFlight(n, flights, src, dest, k);
-        System.out.println("Cheapest flight cost from " + src + " to " + dest + " with at most " + k + " stops is: " + cheapestFlight(n, flights, src, dest, k));
+        int cities[][] = {{0,1,2,3,4},
+                          {1,0,5,0,7},
+                          {2,5,0,6,0},
+                          {3,0,6,0,0},
+                          {4,7,0,0,0}};
+        int totalCost = connectcities(cities);
+        System.out.println("Total cost to connect all cities is: " + totalCost);
     }
 }
